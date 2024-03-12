54c5563b1583e1cf2982e3f3dad7b132d23eb5f1
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-03-11 22:58:59 -0700

    Create first API route

---
+++ lib/Response/ClientError.tsx
@@ -0,0 +1,29 @@
+export default class ClientError {
+    static BadRequest(message: string, content: string|undefined = undefined, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message, content}, {status: 400, statusText: 'Bad Request', headers: headers});
+    }
+
+    static Unauthenticated(message: string, content: string|undefined = undefined, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message, content}, {status: 401, statusText: 'Unauthorized', headers: headers});
+    }
+
+    static Forbidden(message: string, content: string|undefined = undefined, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message, content}, {status: 403, statusText: 'Forbidden', headers: headers});
+    }
+
+    static NotFound(message: string, content: string|undefined = undefined, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message, content}, {status: 404, statusText: 'Not Found', headers: headers});
+    }
+
+    static Conflict(message: string, content: string|undefined = undefined, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message, content}, {status: 409, statusText: 'Conflict', headers: headers});
+    }
+
+    static Gone(message: string, content: string|undefined = undefined, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message, content}, {status: 410, statusText: 'Gone', headers: headers});
+    }
+
+    static PayloadTooLarge(message: string, content: string|undefined = undefined, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message, content}, {status: 413, statusText: 'Payload Too Large', headers: headers});
+    }
+}
---
+++ lib/Response/Success.tsx
@@ -0,0 +1,17 @@
+export default class Success {
+    static OK(message: string, content: string|undefined = undefined, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message, content}, {status: 200, statusText: 'OK', headers: headers});
+    }
+
+    static Created(message: string, location: string, headers: HeadersInit|undefined = undefined) {
+        return Response.json({message}, {status: 201, statusText: 'Created', headers: {...headers, 'Location': location}});
+    }
+
+    static NoContent(headers: HeadersInit|undefined = undefined) {
+        return Response.json({}, {status: 204, statusText: 'No Content', headers: headers});
+    }
+
+    static PartialContent(content: any, headers: HeadersInit|undefined = undefined) {
+        return Response.json(content, {status: 204, statusText: 'No Content', headers: headers});
+    }
+}
