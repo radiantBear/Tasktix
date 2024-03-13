d1e5da668658f4bc05929dcb3017994ba31b5672
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-03-13 13:08:21 -0700

    Allow secure ID generation

---
+++ lib/generateId.tsx
@@ -0,0 +1,14 @@
+export function generateId(length: number = 16): string {
+    const array = new Uint32Array(length);
+    crypto.getRandomValues(array);
+
+    const codeAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        + 'abcdefghijklmnopqrstuvwxyz'
+        + '0123456789';
+
+    let code: string = '';
+    for(let i = 0; i < length; i++)
+        code += codeAlphabet[array[i] % codeAlphabet.length];
+
+    return code;
+}
