a8b000135cb2476d94b78d989406d98df324396a
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-03-14 01:59:42 -0700

    Finish action logic for registering user

--- lib/actions/register.ts
+++ lib/actions/register.ts
@@ -1,14 +1,17 @@
 'use server'
 
 import { validateUsername, validateEmail, validatePassword } from '@/lib/validate';
+import { createUser } from '@/lib/database';
 
 export async function register(username: string, email: string, password: string) {
-  if(!validateUsername)
+  if(!validateUsername(username))
     throw Error('Invalid username');
   if(!validateEmail(email))
     throw Error('Invalid email address');
   if(!validatePassword(password).valid)
     throw Error('Invalid password');
-  
-  return { message: `Registered!` };
+
+  if(createUser(username, email, password))
+    return { message: `Registered!` };
+  throw Error('Could not register user');
 }
---
+++ lib/database/user.ts
@@ -0,0 +1,3 @@
+export function createUser(username: string, email: string, password: string) {
+  return true;
+}
