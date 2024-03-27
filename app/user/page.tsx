52e5318bd5f59e1316d58e629e831a8b48a754eb
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-03-26 22:00:15 -0700

    Require authorization to access user page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,4 +1,8 @@
-export default function Page() {
+import { authorize } from "@/lib/security/authorize";
+
+export default async function Page() {
+  await authorize();
+
   return (
     <h1>Home</h1>
   );
---
+++ lib/security/authorize.ts
@@ -0,0 +1,10 @@
+'use server';
+
+import { redirect } from "next/navigation";
+import { getUser } from "../session";
+
+export async function authorize() {
+  if(!await getUser()) {
+    redirect('/signIn');
+  }
+}
