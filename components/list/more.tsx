63dee296123f3e5332398b0728a3d9711917490a
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-03-29 22:30:27 -0700

    Add list item options

--- components/list/more.tsx
+++ components/list/more.tsx
@@ -1,5 +1,5 @@
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@nextui-org/react';
-import { CalendarEventFill, PeopleFill, Tags, ThreeDots, TrashFill } from 'react-bootstrap-icons';
+import { CalendarEventFill, PeopleFill, StopwatchFill, Tags, ThreeDots, TrashFill } from 'react-bootstrap-icons';
 
 export default function More() {
   return (
@@ -10,7 +10,8 @@ export default function More() {
       <DropdownMenu>
         <DropdownItem key='assign' startContent={<PeopleFill />}>Update assignees</DropdownItem>
         <DropdownItem key='tag' startContent={<Tags />}>Update tags</DropdownItem>
-        <DropdownItem key='tag' startContent={<CalendarEventFill />} className='text-warning' color='warning'>Change due date</DropdownItem>
+        <DropdownItem key='updateExpectedDuration' startContent={<StopwatchFill />}>Updated expected duration</DropdownItem>
+        <DropdownItem key='updateDueDate' startContent={<CalendarEventFill />} className='text-warning' color='warning'>Change due date</DropdownItem>
         <DropdownItem key='remove' startContent={<TrashFill />} className='text-danger' color='danger'>Delete</DropdownItem>
       </DropdownMenu>
     </Dropdown>
