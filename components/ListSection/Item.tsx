afb91fbec3ed0ea30c3e838b3d974de8ac9b6d82
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-04-04 11:19:01 -0700

    Allow updating item completion

--- components/ListSection/Item.tsx
+++ components/ListSection/Item.tsx
@@ -10,9 +10,20 @@ import Priority from './Priority';
 import Users from './Users';
 import { api } from '@/lib/api';
 import { addSnackbar } from '../Snackbar';
+import { ChangeEvent } from 'react';
 
 export default function Item({ item, tagsAvailable, setStatus, deleteItem, addNewTag }: { item: ListItem, tagsAvailable: Tag[], setStatus: (status: ListItem['status']) => void, deleteItem: () => any, addNewTag: (name: string, color: Color) => any }) {  
   const isComplete = item.status == 'Completed';
+  
+  function setComplete(e: ChangeEvent<HTMLInputElement>) {
+    const status = e.target.checked ? 'Completed' : 'Paused';
+    api.patch(`/item/${item.id}`, { status })
+      .then(res => {
+        addSnackbar(res.message, 'success');
+        setStatus(status);
+      })
+      .catch(err => addSnackbar(err.message, 'error'));
+  }
 
   function _deleteItem() {
     api.delete(`/item/${item.id}`)
@@ -26,7 +37,7 @@ export default function Item({ item, tagsAvailable, setStatus, deleteItem, addNe
   return (
     <div className='border-b-1 border-content3 p-4 bg-content1 flex gap-4 items-center justify-between w-full last:border-b-0 last:rounded-b-md'>
       <span className='flex gap-4 items-center justify-start w-full'>
-        <Checkbox isSelected={isComplete} onChange={e => setStatus(e.target.checked ? 'Completed' : 'Paused')} />
+        <Checkbox isSelected={isComplete} onChange={setComplete} />
         <div className='flex flex-col w-64 gap-1'>
           <span className={`text-sm ${isComplete ? 'line-through text-foreground/50' : ''}`}>{item.name}</span>
           <span className={`text-xs ${isComplete ? 'text-secondary/50 line-through' : 'text-secondary'}`}>Due {formatDate(item.dateDue)}</span>
