c6fec233236790da36448b8d0ffd8a5e6bd0d6e4
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-04-04 23:51:28 -0700

    Allow timers to be stopped by completing item

--- components/ListSection/Item.tsx
+++ components/ListSection/Item.tsx
@@ -10,17 +10,89 @@ import Priority from './Priority';
 import Users from './Users';
 import { api } from '@/lib/api';
 import { addSnackbar } from '../Snackbar';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 export default function Item({ item, tagsAvailable, setStatus, deleteItem, addNewTag }: { item: ListItem, tagsAvailable: Tag[], setStatus: (status: ListItem['status']) => void, deleteItem: () => any, addNewTag: (name: string, color: Color) => any }) {  
+  const minute = 1000 * 60;
   const isComplete = item.status == 'Completed';
-  
-  function setComplete(e: ChangeEvent<HTMLInputElement>) {
-    const status = e.target.checked ? 'Completed' : 'Paused';
-    api.patch(`/item/${item.id}`, { status })
+
+  const timer = useRef<NodeJS.Timeout>();
+  const updateTime = useRef(() => {});
+  const lastTime = useRef(new Date());
+  const [elapsedLive, setElapsedLive] = useState(item.elapsedMs + (item.dateStarted ? Date.now() - item.dateStarted.getTime() : 0));
+
+  // Use effect to keep track of the changing timer function
+  useEffect(() => {
+    updateTime.current = () => {
+      console.log(`update: setting timer for ${minute} ms`)
+      timer.current = setTimeout(() => updateTime.current(), minute);
+      setElapsedLive(elapsedLive + (Date.now() - lastTime.current.getTime()));
+      lastTime.current = new Date();
+    }
+  });
+
+  // Start the timer if it should be running when the component is first rendered
+  useEffect(() => {
+    if(item.status == 'In Progress' && !timer.current) {
+      console.log(`onload: setting timer for ${minute - elapsedLive % minute + 5} ms`)
+      timer.current = setTimeout(updateTime.current, minute - elapsedLive % minute + 5);
+    }
+    // Dependencies intentionally excluded to only trigger this when the component is first rendered
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function _stopRunning() {
+    if(timer.current) {
+      clearTimeout(timer.current);
+      timer.current = undefined;
+    }
+  }
+
+  function startRunning() {
+    const startedDate = new Date();
+    api.patch(`/item/${item.id}`, { startTime: startedDate, status: 'In Progress' })
       .then(res => {
         addSnackbar(res.message, 'success');
-        setStatus(status);
+        lastTime.current = startedDate;
+        clearTimeout(timer.current);    // Just for safety
+        console.log(`startRunning: setting timer for ${minute - elapsedLive % minute + 5} ms`)
+        timer.current = setTimeout(updateTime.current, minute - elapsedLive % minute + 5);
+        setStatus('In Progress');
+      })
+      .catch(err => addSnackbar(err.message, 'error'));
+  }
+  
+  function pauseRunning() {
+    const newElapsed = elapsedLive + (Date.now() - lastTime.current.getTime());
+    api.patch(`/item/${item.id}`, { startTime: null, elapsedMs: newElapsed, status: 'Paused' })
+      .then(res => {
+        _stopRunning();
+        addSnackbar(res.message, 'success');
+        setElapsedLive(newElapsed);   // Ensure time is up-to-date since timer was cancelled
+        setStatus('Paused');
+      })
+      .catch(err => addSnackbar(err.message, 'error'));
+  }
+  
+  function setComplete(e: ChangeEvent<HTMLInputElement>) {
+    const newState: { status: 'Completed'|'Paused', startTime?: null, elapsedMs?: number } 
+      = { status: e.target.checked ? 'Completed' : 'Paused' };
+
+    const newElapsed = elapsedLive + (Date.now() - lastTime.current.getTime());
+
+    // Stop timer if going
+    if(e.target.checked && timer.current) {
+      newState.startTime = null;
+      newState.elapsedMs = newElapsed;
+    }
+    
+    api.patch(`/item/${item.id}`, newState)
+      .then(res => {
+        _stopRunning();
+        addSnackbar(res.message, 'success');
+        if(newState.elapsedMs)
+          setElapsedLive(newState.elapsedMs);   // Ensure time is up-to-date since timer was cancelled
+        setStatus(newState.status);
       })
       .catch(err => addSnackbar(err.message, 'error'));
   }
@@ -47,7 +119,7 @@ export default function Item({ item, tagsAvailable, setStatus, deleteItem, addNe
         <Users assignees={item.assignees} isComplete={isComplete} />
       </span>
       <span className='flex gap-4 items-center justify-end'>
-        <Time expected={item.expectedMs} elapsed={item.elapsedMs} dateStarted={item.dateStarted} status={item.status} itemId={item.id} setStatus={setStatus} />
+        <Time expected={item.expectedMs} elapsed={elapsedLive} status={item.status} startRunning={startRunning} pauseRunning={pauseRunning} />
         <More deleteItem={_deleteItem} />
       </span>
     </div>
--- components/ListSection/Time/TimeGroup.tsx
+++ components/ListSection/Time/TimeGroup.tsx
@@ -1,63 +1,9 @@
-import { formatTime, getDateDiff } from '@/lib/date';
+import { formatTime } from '@/lib/date';
 import TimeButton from './TimeButton';
 import ListItem from '@/lib/model/listItem';
-import { useEffect, useRef, useState } from 'react';
-import { api } from '@/lib/api';
-import { addSnackbar } from '@/components/Snackbar';
 
-export default function TimeGroup({ expected, elapsed, dateStarted, status, itemId, setStatus }: { expected: number, elapsed: number, status: ListItem['status'], dateStarted: Date|null, itemId: string, setStatus: (status: ListItem['status']) => void }) {
-  const minute = 1000 * 60;
+export default function TimeGroup({ expected, elapsed, status, startRunning, pauseRunning }: { expected: number, elapsed: number, status: ListItem['status'], startRunning: () => any, pauseRunning: () => any }) {
   const isComplete = status == 'Completed';
-  
-  const timer = useRef<NodeJS.Timeout>();
-  const updateTime = useRef(() => {});
-  const lastTime = useRef(dateStarted || new Date());
-  const [elapsedLive, setElapsedLive] = useState(elapsed + (dateStarted ? Date.now() - dateStarted.getTime() : 0));
-
-  // Stop timer if checkbox was pressed
-  if(isComplete)
-    stopRunning();
-
-  useEffect(() => {
-    updateTime.current = () => {
-      timer.current = setTimeout(() => updateTime.current(), minute);
-      setElapsedLive(elapsedLive + (Date.now() - lastTime.current.getTime()));
-      lastTime.current = new Date();
-    }
-  });
-
-  function stopRunning() {
-    if(timer.current) {
-      clearTimeout(timer.current);
-      timer.current = undefined;
-      if(lastTime.current)
-        setElapsedLive(elapsedLive + (Date.now() - lastTime.current.getTime()));
-    }
-  }
-
-  function startRunning() {
-    api.patch(`/item/${itemId}`, { startTime: new Date(), status: 'In Progress' })
-      .then(res => {
-        addSnackbar(res.message, 'success');
-        lastTime.current = new Date();
-        clearTimeout(timer.current);
-        timer.current = setTimeout(updateTime.current, minute - elapsedLive % minute + 5);
-        setStatus('In Progress');
-      })
-      .catch(err => addSnackbar(err.message, 'error'));
-  }
-  
-  function pauseRunning() {
-    const newElapsed = elapsedLive + (Date.now() - lastTime.current.getTime());
-    api.patch(`/item/${itemId}`, { startTime: null, elapsedMs: newElapsed, status: 'Paused' })
-      .then(res => {
-        addSnackbar(res.message, 'success');
-        stopRunning();
-        setElapsedLive(newElapsed);   // Ensure any delay doesn't result in inaccurate time display
-        setStatus('Paused');
-      })
-      .catch(err => addSnackbar(err.message, 'error'));
-  }
 
   return (
     <>
@@ -69,7 +15,7 @@ export default function TimeGroup({ expected, elapsed, dateStarted, status, item
         <span className='border-r-1 border-content3'></span>
         <span className='flex flex-col items-center'>
           <p className={`text-xs${isComplete ? ' text-foreground/50' : ''}`}>Elapsed</p>
-          <p className={`text-sm${isComplete ? ' text-foreground/50' : ''}`}>{formatTime(elapsedLive)}</p>
+          <p className={`text-sm${isComplete ? ' text-foreground/50' : ''}`}>{formatTime(elapsed)}</p>
         </span>
       </span>
       <TimeButton status={status} startRunning={startRunning} pauseRunning={pauseRunning} />
