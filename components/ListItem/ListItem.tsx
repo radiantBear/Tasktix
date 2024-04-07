561b84fae68094ffc4581580f436b993fff52dcb
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-04-06 22:18:58 -0700

    Fix spacing next to checkbox

--- components/ListItem/ListItem.tsx
+++ components/ListItem/ListItem.tsx
@@ -125,7 +125,7 @@ export default function ListItem({ item, tagsAvailable, setStatus, setCompleted,
   return (
     <div className='border-b-1 border-content3 p-4 bg-content1 flex gap-4 items-center justify-between w-full last:border-b-0'>
       <span className='flex gap-4 items-center justify-start w-full'>
-        <Checkbox tabIndex={0} isSelected={isComplete} onChange={setComplete} />
+        <Checkbox tabIndex={0} isSelected={isComplete} onChange={setComplete} className='-mr-3' />
         <div className='flex flex-col w-64 gap-1 -mt-3 -mb-1'>
           {
             isComplete 
