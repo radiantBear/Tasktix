956e7e800485b9e6332b004993fa58f19835e638
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-04-07 22:28:08 -0700

    Update tag group to use color util fns

--- components/ListItem/Tags.tsx
+++ components/ListItem/Tags.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { api } from '@/lib/api';
-import { getTextColor } from '@/lib/color';
+import { getBackgroundColor, getTextColor } from '@/lib/color';
 import TagModel from '@/lib/model/tag';
 import { Button, Chip, Card, Input, Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react';
 import { useState } from 'react';
@@ -75,7 +75,7 @@ export default function Tags({ itemId, initialTags, isComplete, tagsAvailable, a
           <Card tabIndex={isComplete ? 1 : 0} className={`px-4 basis-1/6 grow shrink flex flex-row items-center justify-start overflow-hidden flex-nowrap h-10 shadow-none cursor-pointer ${isComplete ? 'opacity-50' : 'hover:bg-foreground/10 focus:z-10 focus:outline-2 focus:outline-focus focus:outline-offset-2'}`}>
             <TagsIcon className='shrink-0' />
             <span className='ml-2 flex flex-row items-center justify-start overflow-hidden flex-nowrap'>
-              {tags.map(tag => <Tag key={tag.id} tag={tag} />)}
+              {tags.map(tag => <Chip key={tag.id} variant='dot' size='sm' classNames={{dot: getBackgroundColor(tag.color), base: 'border-0', content: getTextColor(tag.color)}}>{tag.name}</Chip>)}
             </span>
           </Card>
         </PopoverTrigger>
@@ -107,82 +107,4 @@ export default function Tags({ itemId, initialTags, isComplete, tagsAvailable, a
         </PopoverContent>
       </Popover>
   );
-}
-
-function Tag({ tag }: { tag: TagModel }) {
-  let textColor: string;
-  switch(tag.color) {
-    case 'Pink':
-      textColor = 'text-pink-500';
-      break;
-    case 'Red':
-      textColor = 'text-red-500';
-      break;
-    case 'Orange':
-      textColor = 'text-orange-500';
-      break;
-    case 'Amber':
-      textColor = 'text-amber-500';
-      break;
-    case 'Yellow':
-      textColor = 'text-yellow-500';
-      break;
-    case 'Lime':
-      textColor = 'text-lime-500';
-      break;
-    case 'Green':
-      textColor = 'text-green-500';
-      break;
-    case 'Emerald':
-      textColor = 'text-emerald-500';
-      break;
-    case 'Cyan':
-      textColor = 'text-cyan-500';
-      break;
-    case 'Blue':
-      textColor = 'text-blue-500';
-      break;
-    case 'Violet':
-      textColor = 'text-violet-500';
-      break;
-  }
-
-  let bgColor: string;
-  switch(tag.color) {
-    case 'Pink':
-      bgColor = 'bg-pink-500';
-      break;
-    case 'Red':
-      bgColor = 'bg-red-500';
-      break;
-    case 'Orange':
-      bgColor = 'bg-orange-500';
-      break;
-    case 'Amber':
-      bgColor = 'bg-amber-500';
-      break;
-    case 'Yellow':
-      bgColor = 'bg-yellow-500';
-      break;
-    case 'Lime':
-      bgColor = 'bg-lime-500';
-      break;
-    case 'Green':
-      bgColor = 'bg-green-500';
-      break;
-    case 'Emerald':
-      bgColor = 'bg-emerald-500';
-      break;
-    case 'Cyan':
-      bgColor = 'bg-cyan-500';
-      break;
-    case 'Blue':
-      bgColor = 'bg-blue-500';
-      break;
-    case 'Violet':
-      bgColor = 'bg-violet-500';
-      break;
-  }
-
-  return <Chip variant='dot' size='sm' classNames={{dot: bgColor, base: 'border-0', content: textColor}}>{tag.name}</Chip>;
 }
