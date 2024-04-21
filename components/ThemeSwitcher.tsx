ee0ad93da8c8ebde9b73f27a3ad63ccebfcad2f9
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-04-21 12:12:37 -0700

    Fix theme button showing wrong icon

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,19 +1,31 @@
 import { Button, Listbox, ListboxItem, Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react";
 import { useTheme } from "next-themes";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Display, MoonFill, SunFill } from "react-bootstrap-icons";
 import { clearTimeout, setTimeout } from "timers";
 
 export default function ThemeSwitcher() {
-  const [isOpen, setIsOpen] = useState(false);
   const timer = useRef<NodeJS.Timeout>();
   const { theme, setTheme } = useTheme();
-  
-  const currentTheme = theme != 'system' 
-    ? theme 
-    : document.getElementsByTagName('html')[0].classList.contains('light')
-      ? 'light'
-      : 'dark';
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  const [themeIcon, setThemeIcon] = useState(
+    theme != 'system' 
+      ? theme 
+      : window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light'
+  );
+
+  useEffect(() => {
+    setThemeIcon(theme != 'system' 
+      ? theme 
+      : window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light')
+  }, [theme]);
+
+  useEffect(() => setIsMounted(true), []);
 
   function handleMouse(isHovering: boolean) {
     if(isHovering) {
@@ -27,16 +39,16 @@ export default function ThemeSwitcher() {
   return (
     <Popover isOpen={isOpen}>
       <PopoverTrigger onMouseOver={() => handleMouse(true)} onMouseLeave={() => handleMouse(false)}>
-        <Button onClick={() => setTheme(currentTheme == 'dark' ? 'light' : 'dark')} variant='ghost' isIconOnly>
+        <Button onClick={() => setTheme(themeIcon == 'dark' ? 'light' : 'dark')} aria-label={`Set ${themeIcon == 'dark' ? 'light' : 'dark'} theme`} variant='ghost' isIconOnly>
           {
-            currentTheme == 'dark'
+            isMounted && themeIcon == 'dark'
               ? <SunFill />
               : <MoonFill />
           }
         </Button>
       </PopoverTrigger>
       <PopoverContent onMouseOver={() => handleMouse(true)} onMouseLeave={() => handleMouse(false)}>
-        <Listbox selectionMode='single' selectedKeys={[ theme || 'system' ]}>
+        <Listbox selectionMode='single' selectedKeys={[ theme || 'system' ]} aria-label='Choose theme'>
           <ListboxItem key='light' onPress={() => setTheme('light')} startContent={<SunFill />}>Light</ListboxItem>
           <ListboxItem key='dark' onPress={() => setTheme('dark')} startContent={<MoonFill />}>Dark</ListboxItem>
           <ListboxItem key='system' onPress={() => setTheme('system')} startContent={<Display />}>System</ListboxItem>
