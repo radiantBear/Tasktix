0c18511412e615d8f157be641e36a2acc06b431c
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-10-07 17:38:48 -0700

    Fix bug for lists without tags

--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -16,7 +16,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <main className='p-8 w-full flex flex-col gap-8 overflow-y-scroll'>
-      {list && <List startingList={JSON.stringify(list)} startingTagsAvailable={JSON.stringify(tagsAvailable)} />}
+      {list && <List startingList={JSON.stringify(list)} startingTagsAvailable={JSON.stringify(tagsAvailable || [])} />}
     </main>
   );
 }
