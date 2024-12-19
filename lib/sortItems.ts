1611a4bc1cb5d7b152f15722099b95dddd344180
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-12-19 08:10:42 -0800

    Refactor to remove duplication

--- lib/sortItems.ts
+++ lib/sortItems.ts
@@ -28,19 +28,9 @@ export function sortItemsByIndex(a: ListItem, b: ListItem): number {
 }
 
 export function sortItems(hasTimeTracking: boolean, hasDueDates: boolean, a: ListItem, b: ListItem): number {
-  if(a.dateCompleted && b.dateCompleted) {
-    if(a.dateCompleted < b.dateCompleted)
-      return 1;
-    else if(b.dateCompleted < a.dateCompleted)
-      return -1;
-    else
-      return 0;
-  }
-
-  if(a.status == 'Completed' && b.status != 'Completed')
-    return 1;
-  if(b.status == 'Completed' && a.status != 'Completed')
-    return -1;
+  const completed_order = sortItemsByCompleted(a, b);
+  if (completed_order != 0)
+    return completed_order;
 
   if(hasDueDates) {
     if(!a.dateDue) {
