be8586819f5c8c684e167301e6a8b60eb07ccecd
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-03-08 16:11:59 -0800

    Add tests for api helpers

---
+++ lib/api.test.ts
@@ -0,0 +1,478 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import api from '@/lib/api'; // Adjust the import path as needed
+
+
+function createFetchResponse(status: number, message: string, content: any): Response {
+  return {
+    status,
+    json: jest.fn().mockResolvedValue({ message, content })
+  } as any;
+}
+
+
+beforeEach(() => {
+  if (!globalThis.fetch) {
+    globalThis.fetch = jest.fn();
+  }
+  jest.spyOn(globalThis, 'fetch').mockClear();
+});
+
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+
+describe('get', () => {
+  test('Makes request to the provided API route, prepended with "/api"', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.get('/resource');
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][0]).toBe('/api/resource');
+  });
+
+  test('Makes an HTTP GET request with no body', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.get('/resource');
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][1]).toMatchObject({ method: 'GET', body: undefined });
+  });
+
+  test('Parses the JSON-encoded server response', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    const response = await api.get('/resource');
+
+    expect(response).toEqual({
+      code: 200,
+      message: 'OK',
+      content: 'data'
+    });
+  });
+
+  test('Rejects the promise if fetch fails', async () => {
+    const error = new Error('Exception message');
+    (globalThis.fetch as jest.Mock).mockRejectedValue(error);
+
+    await expect(api.get('/resource')).rejects.toThrow('Exception message');
+  });
+
+  test('Redirects to /signIn when response code is 403 and reject with server response', async () => {
+    const fakeResponse = createFetchResponse(403, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { href: '' }
+    });
+
+    await expect(api.get('/resource')).rejects.toEqual({
+      code: 403,
+      message: 'Server error message',
+      content: null
+    })
+    
+    expect(window.location.href).toBe('/signIn');
+  });
+
+  test('Rejects for error responses with HTTP status codes >= 400 (other than 403)', async () => {
+    const fakeResponse = createFetchResponse(500, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await expect(api.get('/resource')).rejects.toEqual({
+      code: 500,
+      message: 'Server error message',
+      content: null
+    });
+  });
+});
+
+
+describe('post', () => {
+  test('Makes request to the provided API route, prepended with "/api"', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.post('/resource', {});
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][0]).toBe('/api/resource');
+  });
+
+  test('Makes an HTTP POST request with JSON-encoded body by default when `data` is an object', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.post('/resource', { key: 'value' });
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][1]).toMatchObject({ 
+      method: 'POST', 
+      headers: { 'Content-Type': 'application/json' },
+      body: '{"key":"value"}' 
+    });
+  });
+
+  test('Throws an unknown encoding error when `data` is an object and `encodingType` is not JSON', async () => {
+    const data = { key: 'value' };
+    
+    await expect(api.post('/resource', data, 'text/plain')).rejects.toThrow(
+      'Unknown encoding text/plain for object parameter'
+    );
+  });
+
+  test('Makes an HTTP POST request with plaintext body by default when `data` is a string', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.post('/resource', 'data here');
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][1]).toMatchObject({ 
+      method: 'POST', 
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'data here' 
+    });
+  });
+
+
+  test('Makes an HTTP POST request with custom encoding type when when `data` is a string and an encoding is specified', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.post('/resource', 'data here', 'text/somethingCustom');
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][1]).toMatchObject({ 
+      method: 'POST', 
+      headers: { 'Content-Type': 'text/somethingCustom' },
+      body: 'data here' 
+    });
+  });
+
+  test('Parses the server response as JSON', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'postData');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+    const data = { key: 'value' };
+
+    const response = await api.post('/resource', data);
+
+    expect(response).toEqual({
+      code: 200,
+      message: 'OK',
+      content: 'postData'
+    });
+  });
+
+  test('Rejects the promise if fetch fails', async () => {
+    const error = new Error('Exception message');
+    (globalThis.fetch as jest.Mock).mockRejectedValue(error);
+
+    await expect(api.post('/resource', {})).rejects.toThrow('Exception message');
+  });
+
+  test('Redirects to /signIn when response code is 403 and reject with server response', async () => {
+    const fakeResponse = createFetchResponse(403, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { href: '' }
+    });
+
+    await expect(api.post('/resource', {})).rejects.toEqual({
+      code: 403,
+      message: 'Server error message',
+      content: null
+    })
+    
+    expect(window.location.href).toBe('/signIn');
+  });
+
+  test('Rejects for error responses with HTTP status codes >= 400 (other than 403)', async () => {
+    const fakeResponse = createFetchResponse(500, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await expect(api.post('/resource', {})).rejects.toEqual({
+      code: 500,
+      message: 'Server error message',
+      content: null
+    });
+  });
+});
+
+
+describe('put', () => {
+  it('should send a PUT request with JSON stringified data and resolve with server response', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'putData');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+    const data = { key: 'value' };
+
+    const response = await api.put('/resource', data);
+    expect(globalThis.fetch).toHaveBeenCalledWith('/api/resource', {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    expect(response).toEqual({
+      code: 200,
+      message: 'OK',
+      content: 'putData'
+    });
+  });
+
+  test('Makes request to the provided API route, prepended with "/api"', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.put('/resource', {});
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][0]).toBe('/api/resource');
+  });
+
+  test('Makes an HTTP PUT request with JSON-encoded body by default', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.put('/resource', { key: 'value' });
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][1]).toMatchObject({ 
+      method: 'PUT', 
+      headers: { 'Content-Type': 'application/json' },
+      body: '{"key":"value"}' 
+    });
+  });
+
+  test('Throws an unknown encoding error when encodingType is not JSON', async () => {
+    const data = { key: 'value' };
+
+    await expect(api.put('/resource', data, 'text/plain')).rejects.toThrow(
+      'Unknown encoding text/plain for object parameter'
+    );
+  });
+
+  test('Parses the server response as JSON', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'postData');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+    const data = { key: 'value' };
+
+    const response = await api.put('/resource', data);
+
+    expect(response).toEqual({
+      code: 200,
+      message: 'OK',
+      content: 'postData'
+    });
+  });
+
+  test('Rejects the promise if fetch fails', async () => {
+    const error = new Error('Exception message');
+    (globalThis.fetch as jest.Mock).mockRejectedValue(error);
+
+    await expect(api.put('/resource', {})).rejects.toThrow('Exception message');
+  });
+
+  test('Redirects to /signIn when response code is 403 and reject with server response', async () => {
+    const fakeResponse = createFetchResponse(403, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { href: '' }
+    });
+
+    await expect(api.put('/resource', {})).rejects.toEqual({
+      code: 403,
+      message: 'Server error message',
+      content: null
+    })
+    
+    expect(window.location.href).toBe('/signIn');
+  });
+
+  test('Rejects for error responses with HTTP status codes >= 400 (other than 403)', async () => {
+    const fakeResponse = createFetchResponse(500, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await expect(api.put('/resource', {})).rejects.toEqual({
+      code: 500,
+      message: 'Server error message',
+      content: null
+    });
+  });
+});
+
+
+describe('patch', () => {
+  it('should send a PATCH request with JSON stringified data and resolve with server response', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'patchData');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+    const data = { key: 'value' };
+
+    const response = await api.patch('/resource', data);
+    expect(globalThis.fetch).toHaveBeenCalledWith('/api/resource', {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    expect(response).toEqual({
+      code: 200,
+      message: 'OK',
+      content: 'patchData'
+    });
+  });
+
+  test('Makes request to the provided API route, prepended with "/api"', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.patch('/resource', {});
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][0]).toBe('/api/resource');
+  });
+
+  test('Makes an HTTP PATCH request with JSON-encoded body by default', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.patch('/resource', { key: 'value' });
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][1]).toMatchObject({ 
+      method: 'PATCH', 
+      headers: { 'Content-Type': 'application/json' },
+      body: '{"key":"value"}' 
+    });
+  });
+
+  test('Throws an unknown encoding error when encodingType is not JSON', async () => {
+    const data = { key: 'value' };
+
+    await expect(api.patch('/resource', data, 'text/plain')).rejects.toThrow(
+      'Unknown encoding text/plain for object parameter'
+    );
+  });
+
+  test('Parses the server response as JSON', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'patchData');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+    const data = { key: 'value' };
+
+    const response = await api.patch('/resource', data);
+
+    expect(response).toEqual({
+      code: 200,
+      message: 'OK',
+      content: 'patchData'
+    });
+  });
+
+  test('Rejects the promise if fetch fails', async () => {
+    const error = new Error('Exception message');
+    (globalThis.fetch as jest.Mock).mockRejectedValue(error);
+
+    await expect(api.patch('/resource', {})).rejects.toThrow('Exception message');
+  });
+
+  test('Redirects to /signIn when response code is 403 and reject with server response', async () => {
+    const fakeResponse = createFetchResponse(403, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { href: '' }
+    });
+
+    await expect(api.patch('/resource', {})).rejects.toEqual({
+      code: 403,
+      message: 'Server error message',
+      content: null
+    })
+    
+    expect(window.location.href).toBe('/signIn');
+  });
+
+  test('Rejects for error responses with HTTP status codes >= 400 (other than 403)', async () => {
+    const fakeResponse = createFetchResponse(500, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await expect(api.patch('/resource', {})).rejects.toEqual({
+      code: 500,
+      message: 'Server error message',
+      content: null
+    });
+  });
+});
+
+
+describe('delete', () => {
+  test('Makes request to the provided API route, prepended with "/api"', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.delete('/resource');
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][0]).toBe('/api/resource');
+  });
+
+  test('Makes an HTTP GET request with no body', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await api.delete('/resource');
+
+    expect((globalThis.fetch as jest.Mock).mock.calls[0][1]).toMatchObject({ method: 'DELETE', body: undefined });
+  });
+
+  test('Parses the JSON-encoded server response', async () => {
+    const fakeResponse = createFetchResponse(200, 'OK', 'data');
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    const response = await api.delete('/resource');
+
+    expect(response).toEqual({
+      code: 200,
+      message: 'OK',
+      content: 'data'
+    });
+  });
+
+  test('Rejects the promise if fetch fails', async () => {
+    const error = new Error('Exception message');
+    (globalThis.fetch as jest.Mock).mockRejectedValue(error);
+
+    await expect(api.delete('/resource')).rejects.toThrow('Exception message');
+  });
+
+  test('Redirects to /signIn when response code is 403 and reject with server response', async () => {
+    const fakeResponse = createFetchResponse(403, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { href: '' }
+    });
+
+    await expect(api.delete('/resource')).rejects.toEqual({
+      code: 403,
+      message: 'Server error message',
+      content: null
+    })
+    
+    expect(window.location.href).toBe('/signIn');
+  });
+
+  test('Rejects for error responses with HTTP status codes >= 400 (other than 403)', async () => {
+    const fakeResponse = createFetchResponse(500, 'Server error message', null);
+    (globalThis.fetch as jest.Mock).mockResolvedValue(fakeResponse);
+
+    await expect(api.delete('/resource')).rejects.toEqual({
+      code: 500,
+      message: 'Server error message',
+      content: null
+    });
+  });
+});
