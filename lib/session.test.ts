370b89fc61c1455ca88515f63195ff10160606b6
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-08-22 08:05:44 -0700

    Fix final TypeScript errors

--- lib/session.test.ts
+++ lib/session.test.ts
@@ -29,7 +29,11 @@ beforeEach(() => {
 describe('setUser', () => {
   test('Creates a session, sets a cookie with the session ID, and returns the session ID', async () => {
     const userId = 'user123';
-    const mockedCookies: typeof cookies = (cookies as jest.Mock)();
+    const mockedCookies = (cookies as jest.Mock)() as {
+      get: jest.Mock;
+      set: jest.Mock;
+      delete: jest.Mock;
+    };
 
     (createSession as jest.Mock).mockResolvedValue(true);
 
