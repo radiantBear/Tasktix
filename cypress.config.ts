b40c5b825c3adc63506dd162c9dddee1fe453ada
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-09-03 23:34:14 -0700

    Record video of failed Cypress runs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,22 @@
+import fs from 'fs';
+
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  video: true,
   e2e: {
-    baseUrl: 'http://localhost:3000'
-    // setupNodeEvents(on, config) {
-    //   implement node event listeners here
-    // }
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, _) {
+      on('after:run', results => {
+        if (!results) return;
+        if ('status' in results) return; // Only CypressFailedRunResults has a status
+
+        results.runs.forEach(specRun => {
+          if (specRun.stats.failures === 0 && specRun.video) {
+            fs.unlinkSync(specRun.video);
+          }
+        });
+      });
+    }
   }
 });
