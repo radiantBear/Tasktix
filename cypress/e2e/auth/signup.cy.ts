1e6718d2f16723a7c06d7345f60dfb9426d6752c
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-09-03 17:30:10 -0700

    Add first Cypress test

---
+++ cypress/e2e/auth/signup.cy.ts
@@ -0,0 +1,18 @@
+describe('Sign up', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Allows users to sign up', () => {
+    cy.contains('Sign In').click();
+    cy.contains('Sign Up').click();
+
+    cy.findByLabelText('Username').type(`user_${Date.now()}`);
+    cy.findByLabelText('Email').type(`user${Date.now()}@example.com`);
+    cy.findByLabelText('Password').type('password123');
+
+    cy.findAllByRole('button').contains('Sign Up').click();
+
+    cy.location('pathname').should('eq', '/list');
+  });
+});
