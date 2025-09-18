e93cb6f794f0760f80f03976f34b79e76c23dcd2
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-09-18 14:14:52 -0700

    Fix email validation

--- lib/validate.ts
+++ lib/validate.ts
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import { regexes as zodRegexes } from 'zod';
+
 import { namedColorSet } from './model/color';
 
 interface InputResult {
@@ -50,10 +52,8 @@ export function validateUsername(username: string): InputResult {
 
 export function validateEmail(email: string): boolean {
   /* Credit: bortzeyer, https://stackoverflow.com/a/201378 */
-  const emailRegex =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
 
-  return emailRegex.test(email) && email.length <= 128;
+  return zodRegexes.email.test(email) && email.length <= 128;
 }
 
 export function validatePassword(password: string): InputResult {
